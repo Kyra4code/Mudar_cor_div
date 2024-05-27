9b720dd410d000142f6ad025fe269114f0d45273
Kyra4code <lucarioecharmito@gmail.com>
2024-05-27 20:31:08 -0300

    comit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+let verm = document.getElementById("red").value;
+let azu = document.getElementById("blue").value;
+let verd = document.getElementById("green").value;
+let mudanca = document.getElementById("tchuco").value;
+
+function mudarRed(){
+}
